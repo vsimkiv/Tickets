a616a7b7de7135cdcc29d8149d8c27b8e4e45a03
Volodymyr Simkiv <volodymyr.simkiv@gmail.com>
2019-07-30 11:09:44 +0300

    Skeleton with spring security and swagger

---
+++ src/main/java/com/simkiv/tickets/service/AuthService.java
@@ -0,0 +1,83 @@
+package com.simkiv.tickets.service;
+
+import com.simkiv.tickets.dto.JwtAuthenticationResponse;
+import com.simkiv.tickets.dto.LoginRequest;
+import com.simkiv.tickets.dto.SignUpRequest;
+import com.simkiv.tickets.exception.AppException;
+import com.simkiv.tickets.exception.ConflictException;
+import com.simkiv.tickets.model.Role;
+import com.simkiv.tickets.model.RoleName;
+import com.simkiv.tickets.model.User;
+import com.simkiv.tickets.repository.RoleRepository;
+import com.simkiv.tickets.repository.UserRepository;
+import com.simkiv.tickets.security.JwtTokenProvider;
+import com.simkiv.tickets.security.UserPrincipal;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+
+@Service
+@Slf4j
+public class AuthService {
+
+    private AuthenticationManager authenticationManager;
+    private UserRepository userRepository;
+    private RoleRepository roleRepository;
+    private PasswordEncoder passwordEncoder;
+    private JwtTokenProvider tokenProvider;
+
+    @Autowired
+    public AuthService(AuthenticationManager authenticationManager, UserRepository userRepository, RoleRepository roleRepository, PasswordEncoder passwordEncoder, JwtTokenProvider tokenProvider) {
+        this.authenticationManager = authenticationManager;
+        this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.tokenProvider = tokenProvider;
+    }
+
+    public JwtAuthenticationResponse authenticateUser(LoginRequest loginRequest) {
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        loginRequest.getEmail(),
+                        loginRequest.getPassword()
+                )
+        );
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        String jwt = tokenProvider.generateToken(authentication);
+
+        UserPrincipal userPrincipal = (UserPrincipal) authentication.getPrincipal();
+
+        log.info("User with [email: {}] has logged in", userPrincipal.getEmail());
+
+        return new JwtAuthenticationResponse(jwt);
+    }
+
+    public Long registerUser(SignUpRequest signUpRequest) {
+
+        if(userRepository.existsByEmail(signUpRequest.getEmail())) {
+            throw new ConflictException("Email [email: " + signUpRequest.getEmail() + "] is already taken");
+        }
+
+        User user = new User(signUpRequest.getName(), signUpRequest.getEmail(), signUpRequest.getPassword());
+
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+
+        Role userRole = roleRepository.findByName(RoleName.ROLE_USER)
+                .orElseThrow(() -> new AppException("User Role not set. Add default roles to database."));
+
+        user.setRoles(Collections.singleton(userRole));
+
+        log.info("Successfully registered user with [email: {}]", user.getEmail());
+
+        return userRepository.save(user).getId();
+        }
+}
